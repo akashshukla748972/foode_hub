958e8738eb225c8fec0fd8f522b7ef9098ce0fcd
akashshukla748972 <shuklaakash748972@gmail.com>
2025-02-20 02:41:23 +0530

    add banner in the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50">
       <Navbar />
-      <h1 class="text-3xl font-bold underline">Foodi Hub!</h1>
+      <br />
+      <main className="flex-1 w-full">
+        <div className="w-full ">
+          <Banner />
+        </div>
+      </main>
     </div>
   );
 };
