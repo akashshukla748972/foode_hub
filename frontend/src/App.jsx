62b6885fa9056a7c50075728f3ecb327b1997e83
akashshukla748972 <shuklaakash748972@gmail.com>
2025-02-19 23:05:17 +0530

    add responsive navbar for all devices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-r from-red-50 via-orange-50 to-yellow-50">
+      <Navbar />
       <h1 class="text-3xl font-bold underline">Foodi Hub!</h1>
     </div>
   );
